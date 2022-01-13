adc010136a6ed79667672213780863e97f9ccd9f
Konstantinos_Siampanis <kostas.siampa@gmail.com>
2022-01-13 02:17:24 +0200

    initial Scene and Hurricane Oculus set up
    Hurricane and hands garbing works

---
+++ Assets/HurricaneVR/Framework/Integrations/Oculus/HVROculusManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using HurricaneVR.Framework.Core.Player;
+using UnityEngine;
+
+namespace HurricaneVR.Framework.Oculus
+{
+    public class HVROculusManager : MonoBehaviour
+    {
+        public MonoBehaviour[] ComponentsToDisable;
+        public Dictionary<MonoBehaviour, Vector3> Stuff = new Dictionary<MonoBehaviour, Vector3>();
+
+        public void OnEnable()
+        {
+            OVRManager.InputFocusAcquired += OnResume;
+            OVRManager.InputFocusLost += OnPaused;
+        }
+
+     
+
+
+        private void OnResume()
+        {
+            foreach (var c in ComponentsToDisable)
+            {
+                c.enabled = true;
+                c.transform.position = Stuff[c];
+            }
+            Time.timeScale = 1f;
+        }
+
+        private void OnPaused()
+        {
+            foreach (var c in ComponentsToDisable)
+            {
+                var p  = c.transform.position;
+                c.enabled = false;
+                Stuff[c] = c.transform.position;
+                c.transform.position = p;
+            }
+
+            //Time.timeScale = 0f;
+
+        }
+
+        public void OnDisable()
+        {
+            OVRManager.InputFocusAcquired -= OnResume;
+            OVRManager.InputFocusLost -= OnPaused;
+        }
+    }
+}
